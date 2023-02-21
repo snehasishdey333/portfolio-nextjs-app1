8e1d0b74b9948ca92b154d1a5e43bd6bb95352d3
Snehasish-Dey <snehasishdey333@gmail.com>
2023-02-21 23:36:03 +0530

    modified skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -16,17 +16,18 @@ import next from '../public/images/skills/nextjs-icon.png'
 const Skills = () => {
   return (
     <div id="skills" className="w-full p-6 mx-auto my-10 md:px-40">
+   
     <div className='space-y-4 text-center md:text-left md:space-y-6'>
     <h2 className='uppercase text-orange-600'>Skills</h2>
                <h1>What I can do</h1>
     </div>
     
         <div className='w-full p-6 mx-auto md:p-20'>
-    <div className='flex flex-wrap items-center justify-around'>
+    <div className='w-full flex flex-wrap items-center justify-around'>
     {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={html} alt="icon" height="50" width="50"/>
+                <Image src={html} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>HTML</h3>
@@ -34,9 +35,9 @@ const Skills = () => {
         </div>
         
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={css} alt="icon" height="50" width="50"/>
+                <Image src={css} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>CSS</h3>
@@ -44,9 +45,9 @@ const Skills = () => {
         </div>
         
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={javascript} alt="icon" height="50" width="50"/>
+                <Image src={javascript} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>JavaScript</h3>
@@ -54,9 +55,9 @@ const Skills = () => {
         </div>
 
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={java} alt="icon" height="50" width="50"/>
+                <Image src={java} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>Java</h3>
@@ -64,9 +65,9 @@ const Skills = () => {
         </div>
 
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={react} alt="icon" height="50" width="50"/>
+                <Image src={react} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>React JS</h3>
@@ -74,9 +75,9 @@ const Skills = () => {
         </div>
         
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={node} alt="icon" height="50" width="50"/>
+                <Image src={node} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>Node JS</h3>
@@ -84,9 +85,9 @@ const Skills = () => {
         </div>
         
           {/* item 1 */}
-          <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+          <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={next} alt="icon" height="50" width="50"/>
+                <Image src={next} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>NEXT JS</h3>
@@ -94,9 +95,9 @@ const Skills = () => {
         </div>
         
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={tailwind} alt="icon" height="50" width="50"/>
+                <Image src={tailwind} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>Tailwind CSS</h3>
@@ -104,9 +105,9 @@ const Skills = () => {
         </div>
 
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={mysql} alt="icon" height="50" width="50"/>
+                <Image src={mysql} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>MySQL</h3>
@@ -114,9 +115,9 @@ const Skills = () => {
         </div>
         
         {/* item 1 */}
-        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[180px] h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
+        <div className='mx-1 my-3 md:mx-2 md:my-3 lg:mx-3 lg:my-3 flex items-center justify-center px-4 py-3 space-x-4 bg-white shadow-lg rounded-xl w-[150px] h-[60px] md:w-[180px] md:h-[80px] hover:scale-105 duration-800 cursor-pointer ease'>
             <div className='w-2/5'>
-                <Image src={mongodb} alt="icon" height="50" width="50"/>
+                <Image src={mongodb} alt="icon" height="40" width="40"/>
             </div>
             <div>
                 <h3>mongoDB</h3>
